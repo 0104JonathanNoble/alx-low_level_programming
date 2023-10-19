8123c899a449b72e696cfdacef6c1a02a1bdef54
0104JonathanNoble <noblejonathan450@gmail.com>
2023-10-19 01:15:47 -0700

    more_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - concatenate two strings
+ * dest return pointer
+ * n number of bytes
+ * Return: void
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++);
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[i + 1] = src[j];
+	dest[i + j] = '\0';
+	return (dest);
+}
