d9513a8bbe23dd74e914d53e3986be53c8f7a783
0104JonathanNoble <noblejonathan450@gmail.com>
2023-10-19 14:14:23 -0700

    more_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,11 +7,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+        int i;
+        int j;
 
-	for (i = 0; dest[i] != '\0'; i++);
-	for (j = 0; j < n && src[j] != '\0'; j++)
-		dest[i + 1] = src[j];
-	dest[i + j] = '\0';
-	return (dest);
+        i = 0;
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        j = 0;
+        while (j < n && src[j] != '\0')
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+        dest[i] = '\0';
+        return (dest);
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,4 +4,32 @@
  * separate words
  * Return: string
  */
-char *cap_string(char *)
+char *cap_string(char *a)
+{
+	int i, j;
+	int o[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	i = 0;
+	while (*(a + i) != '\0')
+	{
+		if (*(a + i) >= 'a' && *(a + i) <= 'z')
+		{
+			if (i == 0)
+		{
+			*(a + i) = *(a + i) - 32;
+		}
+		else
+		{
+			for (j = 0; j <= 32; j++)
+			{
+				if (o[j] == *(a + i -1))
+				{
+					*(a + i) = *(a + i) - 32;
+				}
+			}
+		}
+		}
+		i++;
+	}
+	return (a);
+}
