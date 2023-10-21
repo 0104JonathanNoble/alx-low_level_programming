3a915c7c0edc2e7abf5e25a294c1416d53cd9808
0104JonathanNoble <noblejonathan450@gmail.com>
2023-10-21 01:49:38 -0700

    loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,23 @@
 #include "main.h"
 /**
- * main - print sign of a number
+ * print_sign - print sign of a number
+ * @n: declared number
  */
 int print_sign(int n)
 {
 	if (n > 0);
 	{
 		_putchar('+');
-		return (1)
+		return (1);
 	}
 	else (n == 0);
+	{
+		_putchar('0');
+		return (0);
+	}
+	else (n < 0);
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
