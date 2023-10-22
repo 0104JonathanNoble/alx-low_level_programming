6f102b551c86fb25bafbb195e589550f1ade0cb7
0104JonathanNoble <noblejonathan450@gmail.com>
2023-10-22 07:25:30 -0700

    more_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int main(void)
 
 	for (i = 1;i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if ((i % 3) == 0 && (i % 5) ==0)
 			printf("FizzBuzz");
-		else if (i % 3 == 0)
+		else if ((i % 3) == 0)
 			printf("Fizz");
-		else if (i % 5 == 0)
-			printf9("Buzz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
 		else
 			printf("%i", i);
-		if (i < 100)
+		if (i == 100)
 			printf(" ");
 	}
 	printf("\n");
