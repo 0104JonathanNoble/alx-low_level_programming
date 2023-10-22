75dd3e0fde9df8fe1b5dca5dfc3b8f8d409eaba4
0104JonathanNoble <noblejonathan450@gmail.com>
2023-10-22 07:10:29 -0700

    more_loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _isdigit - search digit 0 to 9
+ * @c: int to be checked
  * Return: 1 if c is a digit, 0 if otherwise
  */
 int _isdigit(int c)
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
  * mul - multiplies two integers
- * Return: output of multiple
+ * @a: int variable
+ * @b: int variable
+ * Return: Always 0
  */
 int mul(int a, int b)
 {
