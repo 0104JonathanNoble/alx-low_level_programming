2f23e43ef2e16b977c6ebfdc1312f2c9ba1b9e8b
0104JonathanNoble <noblejonathan450@gmail.com>
2023-10-22 01:51:53 -0700

    loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,29 +6,33 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; j < 10; i++)
+	for (i = 0; j <= 9; i++)
 	{
-		for (j = 0; j <10; j++)
+		for (j = 0; j <= 9; j++)
 		{
-		k = j * i;
-		if (j == 0)
-		{
-			_putchar(k + '0');
+			k = i + j;
+
+			if ((k / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		if (k < 10 && j != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
-		} else if (k >= 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
 }
