d1bcbf511d2722a1b562ce856137b5e6b0cca793
0104JonathanNoble <noblejonathan450@gmail.com>
2023-10-26 12:09:43 -0700

    recursions

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,3 +5,17 @@
  * Return: square root
  */
 int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+int _sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
