0741e85f3a029455c4b75243e88f583d299cac45
0104JonathanNoble <noblejonathan450@gmail.com>
2023-12-01 03:53:57 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 		text_content = "";
-	for (j = 0, text_content[j], j++)
+	for (j = 0; text_content[j]; j++)
 		;
 
 	k = write(i, text_content, j);
