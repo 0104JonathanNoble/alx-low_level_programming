d2e29559781eaa0042c1e1797a11e40331839ab8
0104JonathanNoble <noblejonathan450@gmail.com>
2023-12-01 03:37:36 -0800

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (j = 0; text_content(j); j++)
+		for (j = 0; text_content[j]; j++)
 			;
 		k = write(i, text_content, j);
 
